8ef7a2343f1d6f6495d9b28e7d26191091faf9c9
JHon <jchafla@institutmvm.cat>
2023-05-11 19:12:42 +0200

    Mosificaciones en ambos documentos

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
+
 <?php
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 include "hola.php"
 echo "listadp de personas:\n";
 include "persona.php";
 include "persona2.php";
+include "persona3.php";
 ?>
